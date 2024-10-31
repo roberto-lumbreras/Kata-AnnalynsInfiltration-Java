45491f4c3a6e17085bf736cdbe6166c9e1cce92b
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-10-31 11:19:43 +0100

    removed checkstyle plugin from pom and implemented methods

--- src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
+++ src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
@@ -4,19 +4,23 @@ public class AnnalynsInfiltration {
 
     public static boolean canFastAttack(boolean knightIsAwake) {
         // Implement method
+        return !knightIsAwake;
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
         // Implement method
+        return knightIsAwake||archerIsAwake||prisonerIsAwake;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
         // Implement method
+        return !archerIsAwake&&prisonerIsAwake;
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
             boolean petDogIsPresent) {
         // Implement method
+        return (petDogIsPresent&&!archerIsAwake)||(!petDogIsPresent&&prisonerIsAwake&&!knightIsAwake&&!archerIsAwake);
     }
 
 }
